4a2e7a5815100dc501bbfe4bbb3acd0573056770
Leonardo Ferreira <leonardoferreiraprogramador@gmail.com>
2023-06-27 11:47:24 -0300

    feat: Icon Component

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Text from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Button} from './src/components/Button/Button';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
@@ -12,10 +13,13 @@ function App(): JSX.Element {
         <Text preset="headingLarge" italic style={{marginBottom: 16}}>
           React Native
         </Text>
-        <Button title="Primary" marginBottom="s12" />
+        {/* <Button title="Primary" marginBottom="s12" />
         <Button title="Primary Disabled" marginBottom="s12" disabled />
         <Button title="Outline" marginBottom="s12" preset="outline" />
-        <Button title="Outiline Disabled" preset="outline" disabled />
+        <Button title="Outiline Disabled" preset="outline" disabled /> */}
+        <Icon name='arrowLeft' color='backgroundContrast' size={50}/>
+        <Icon name='arrowRight' color='carrotSecondary' size={50}/>
+        <Icon name='chatOn' color='buttonPrimary' size={50}/>
       </SafeAreaView>
     </ThemeProvider>
   );
--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -5,13 +5,13 @@ import {
 } from 'react-native';
 
 import {ThemeColors} from '../../theme/theme';
-import {useTheme} from '@shopify/restyle';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
   color: ThemeColors;
 }
 export function ActivityIndicator({color}: Props) {
-  const {colors} = useTheme();
+  const {colors} = useAppTheme();
 
   return <RNActivityIndicator color={colors[color]} />;
 }
