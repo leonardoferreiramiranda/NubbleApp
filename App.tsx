201f7696709e598638469d7adc39a70391693d78
Leonardo Ferreira <leonardoferreiraprogramador@gmail.com>
2023-06-30 13:15:52 -0300

    feat: TextInput Component

--- App.tsx
+++ App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import Text from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView style={{padding: 16}}>
-        <Text preset="headingLarge" italic style={{marginBottom: 16}}>
-          React Native
-        </Text>
-        {/* <Button title="Primary" marginBottom="s12" />
-        <Button title="Primary Disabled" marginBottom="s12" disabled />
-        <Button title="Outline" marginBottom="s12" preset="outline" />
-        <Button title="Outiline Disabled" preset="outline" disabled /> */}
-        <Icon name='arrowLeft' color='backgroundContrast' size={50}/>
-        <Icon name='arrowRight' color='carrotSecondary' size={50}/>
-        <Icon name='chatOn' color='buttonPrimary' size={50}/>
+        <View style={{paddingHorizontal: 24}}>
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá!
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <TextInput
+            errorMessage="mensagem de erro"
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
+
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            RightComponent={<Icon name="eyeOn" color="gray2" />}
+            boxProps={{mb: 's10'}}
+          />
+
+          <Text color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button title="Entrar" mt="s48" />
+          <Button preset="outline" title="Criar uma conta" mt="s12" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,75 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+  TextStyle,
+} from 'react-native';
+import {Box, BoxProps} from '../Box/Box';
+import Text, {$fontFamily, $fontSizes} from '../Text/Text';
+import {useAppTheme} from '../../hooks/useAppTheme';
+
+interface TextInputProps extends RNTextInputProps {
+  label: string;
+  errorMessage?: string;
+  RightComponent?: React.ReactElement;
+  boxProps?: BoxProps;
+}
+export function TextInput({
+  label,
+  errorMessage,
+  RightComponent,
+  boxProps,
+  ...rnTextInputProps
+}: TextInputProps) {
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  const $textInputContainer: BoxProps = {
+    flexDirection: 'row',
+    borderWidth: errorMessage ? 2 : 1,
+    borderColor: errorMessage ? 'error' : 'gray4',
+    padding: 's16',
+    borderRadius: 's12',
+  };
+
+  return (
+    <Box {...boxProps}>
+      <Pressable onPress={focusInput}>
+        <Text preset="paragraphMedium" mb="s4">
+          {label}
+        </Text>
+        <Box {...$textInputContainer}>
+          <RNTextInput
+            ref={inputRef}
+            placeholderTextColor={colors.gray2}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+          {RightComponent && (
+            <Box justifyContent="center" ml="s16">
+              {RightComponent}
+            </Box>
+          )}
+        </Box>
+        {errorMessage && (
+          <Text color="error" preset="paragraphSmall" bold>
+            {errorMessage}
+          </Text>
+        )}
+      </Pressable>
+    </Box>
+  );
+}
+
+const $textInputStyle: TextStyle = {
+  flexGrow: 1,
+  flexShrink: 1,
+  padding: 0,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
