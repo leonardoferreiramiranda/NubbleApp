2a06c8657dbb60ff4c292adba5e55e63cf731b04
Leonardo Ferreira <leonardoferreiraprogramador@gmail.com>
2024-02-13 18:20:29 -0300

    feat: Teste de Componentes com a React Native Testing Library

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'react-native',
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleDirectories: ['node_modules', './src/test'],
+  modulePathIgnorePatterns: ['.*/mockedData/.*'],
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
 };
