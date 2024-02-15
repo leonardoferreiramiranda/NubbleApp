050f787c7ddb77215fb73b5ee472603d27dfe7cd
Leonardo Ferreira <leonardoferreiraprogramador@gmail.com>
2024-02-15 11:29:32 -0300

    feat: Testando React Hook e Aprofundando em Mocks

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'react-native',
-  collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+  ],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
