a4bf8e5538bfa35330c167dd22cde4a11ba59da7
Leonardo Ferreira <leonardoferreiraprogramador@gmail.com>
2024-03-29 11:30:25 -0300

    feat: 26 - Fluxo de Onboarding

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {useAuthCredentials} from '@services';
 
 import {ActivityIndicator, Box} from '@components';
 
 import {AppStack} from './AppStack';
 import {AuthStack} from './AuthStack';
+import {OnboardingStack} from './OnboardingStack';
+import {Stacks, useRouter} from './useRouter';
+
+function LoadingScreen() {
+  return (
+    <Box
+      flex={1}
+      backgroundColor="background"
+      justifyContent="center"
+      alignItems="center">
+      <ActivityIndicator size="large" />
+    </Box>
+  );
+}
+
+const stacks: Record<Stacks, React.ReactElement> = {
+  Loading: <LoadingScreen />,
+  Auth: <AuthStack />,
+  App: <AppStack />,
+  Onboarding: <OnboardingStack />,
+};
 
 export function Router() {
-  const {authCredentials, isLoading} = useAuthCredentials();
+  const stack = useRouter();
 
-  if (isLoading) {
-    return (
-      <Box
-        flex={1}
-        backgroundColor="background"
-        justifyContent="center"
-        alignItems="center">
-        <ActivityIndicator size="large" />
-      </Box>
-    );
-  }
-  return (
-    <NavigationContainer>
-      {authCredentials ? <AppStack /> : <AuthStack />}
-    </NavigationContainer>
-  );
+  const Stack = stacks[stack];
+
+  return <NavigationContainer>{Stack}</NavigationContainer>;
 }
